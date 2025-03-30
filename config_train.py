5bf0f5cc7e0726f2fc1c52b30f6d942b7edc6496
DejanGjer <dejangjer@gmail.com>
2025-03-30 19:44:44 +0200

    Added training configuration.

---
+++ config_train.py
@@ -0,0 +1,6 @@
+learning_rate = 0.1
+momentum = 0.9
+decay = 5e-3
+epochs = 50
+batch_size = 768
+milestones = [30, 40]
--- train.py
+++ train.py
@@ -4,16 +4,11 @@ import torch
 import torch.optim as optim
 import torch.nn.functional as F
 
+import config_train as config
 from resnet18 import ResNet18
 from dataset import cifar10_loader_resnet, transform_base_train, transform_base_test
+from utils import set_compute_device
 
-
-LR = 0.1
-MOM = 0.9
-DECAY = 5e-3
-EPOCHS = 50
-BATCH_SIZE = 768
-MILESTONES = [30, 40]
 
 def train(model, device, train_loader, optimizer, epoch, loss_fn):
     model.train()
@@ -58,29 +53,44 @@ def test(model, device, test_loader, loss_fn, dataset_name=None, filter_map=None
 
     return index_store
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-print(device)
-
-loader = cifar10_loader_resnet
-
-train_loader = loader(device, BATCH_SIZE, transform_base_train, train=True)
-test_loader = loader(device, BATCH_SIZE, transform_base_test)
-
-model_normal = ResNet18('normal').to(device)
-
-# ---- Normal net:
-
-optimizer_normal = optim.SGD(filter(lambda p: p.requires_grad,
-                             model_normal.parameters()), lr=LR, momentum=MOM, weight_decay=DECAY)
-scheduler = optim.lr_scheduler.MultiStepLR(optimizer_normal, milestones=MILESTONES, gamma=0.1)
-start_time = time.time()
+def print_training_info(device):
+    print("Configuration details:")
+    print(f"Learning rate: {config.learning_rate}")
+    print(f"Momentum: {config.momentum}")
+    print(f"Weight decay: {config.decay}")
+    print(f"Batch size: {config.batch_size}")
+    print(f"Epochs: {config.epochs}")
+    print(f"Milestones: {config.milestones}")
+    print("---------------------------------")
+    print(f"Device used for training: {device}")
 
 
-for epoch in range(1, EPOCHS + 1):
-    train(model_normal, device, train_loader, optimizer_normal,
-          epoch, loss_fn=F.cross_entropy)
-    scheduler.step()
+if __name__ == "__main__":
+    # Set device
+    device = set_compute_device()
 
-model_normal.freeze()
-torch.save(model_normal, 'model_normal.pt')
+    print_training_info(device)
+
+    loader = cifar10_loader_resnet
+
+    train_loader = loader(device, config.batch_size, transform_base_train, train=True)
+    test_loader = loader(device, config.batch_size, transform_base_test)
+
+    model_normal = ResNet18('normal').to(device)
+
+    # ---- Normal net:
+
+    optimizer_normal = optim.SGD(filter(lambda p: p.requires_grad,
+                                model_normal.parameters()), lr=config.learning_rate, momentum=config.momentum, weight_decay=config.decay)
+    scheduler = optim.lr_scheduler.MultiStepLR(optimizer_normal, milestones=config.milestones, gamma=0.1)
+    start_time = time.time()
+
+
+    for epoch in range(1, config.epochs + 1):
+        train(model_normal, device, train_loader, optimizer_normal,
+            epoch, loss_fn=F.cross_entropy)
+        scheduler.step()
+
+    model_normal.freeze()
+    torch.save(model_normal, 'model_normal.pt')
 
